ea1930141d19df3836fcad86232ce59981c5f589
tibelf <tibelf@tibelf-Lenovo-IdeaPad-Y470.(none)>
2013-07-03 13:11:31 +0800

    init

---
+++ src/main/java/com/jd/samples/HelloServlet.java
@@ -0,0 +1,22 @@
+package com.jd.samples;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/plain");
+		response.setStatus(200);
+		PrintWriter writer = response.getWriter();
+		writer.println("Hello from JAE");
+		writer.close();
+	}
+}
